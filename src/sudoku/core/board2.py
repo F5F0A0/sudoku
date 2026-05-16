268c73aec5aa2f437d58fa864ed5968e48583821
F5F0A0 <42.741.42@gmail.com>
2026-05-16 02:16:53 -0400

    update cell2 and board2

--- src/sudoku/core/board2.py
+++ src/sudoku/core/board2.py
@@ -5,13 +5,22 @@ class Board2:
     def __init__(self, box_size=3):
         self.box_size = box_size
         self.size = box_size**2
-        self.board = [Cell2() for _ in range(self.size**2)]
+        self.cells = [Cell2() for _ in range(self.size**2)]
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
 
     def _index(self, row, col):
         return row * self.size + col
 
     def get_cell(self, row, col):
-        return self.board[self._index(row, col)]
+        if 0 <= row < self.size and 0 <= col < self.size:
+            return self.cells[self._index(row, col)]
+        else:
+            raise IndexError("Index {self._index} ({row}, {col}) out of range.")
 
     def set_value(self, row, col, value):
         self.get_cell(row, col).value = value
@@ -19,7 +28,7 @@ class Board2:
     def get_row(self, row_index):
         start = row_index * self.size
         stop = start + self.size
-        return self.board[start:stop]
+        return self.cells[start:stop]
 
     def get_col(self, col_index):
         pass
--- src/sudoku/core/cell2.py
+++ src/sudoku/core/cell2.py
@@ -1,7 +1,13 @@
 class Cell2:
     def __init__(
-        self, value: int = 0, is_given: bool = False, candidates: set | None = None
+        self, value: int = 0, is_given: bool = False, candidates: set[int] | None = None
     ):
         self.value = value
         self.is_given = is_given
         self.candidates = candidates if candidates is not None else set()
+
+    def __repr__(self):
+        return "Cell(value={self.value}, given={self.given})"
+
+    def __eq__(self, value):
+        return self.value == value
